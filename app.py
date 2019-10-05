42911566ccd48298a34e3c7027755afba905a06f
FritzHeider <fritz.heider@students.makeschool.com>
2019-10-04 22:53:53 -0700

    ok

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from pymongo import MongoClient
+import os
+from bson.objectid import ObjectId
+from datetime import datetime
+
 
 app = Flask(__name__)
 
@@ -6,3 +11,12 @@ app = Flask(__name__)
 def index():
     """Return homepage."""
     return render_template('home.html', msg='What are we selling!')
+
+items = [
+    { 'title': 'Cat Videos', 'description': 'Cats acting weird', 'img': 'img src="https://via.placeholder.com/150' },
+    { 'title': '80\'s Music', 'description': 'Don\'t stop believing!' }
+]
+@app.route('/items')
+def itemss_index():
+    """Show all items."""
+    return render_template('items.html', items=items)
