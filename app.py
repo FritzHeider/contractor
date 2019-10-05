ad7d44525252d2d31cfbcca1a7c9ba46771ccdc8
FritzHeider <fritz.heider@students.makeschool.com>
2019-10-05 00:34:35 -0700

    slwoly but surely

--- app.py
+++ app.py
@@ -13,10 +13,15 @@ def index():
     return render_template('home.html', msg='What are we selling!')
 
 items = [
-    { 'title': 'Cat Videos', 'description': 'Cats acting weird', 'img': 'img src="https://via.placeholder.com/150' },
+    { 'title': 'Cat Videos', 'description': 'Cats acting weird', 'img': 'img src="https://via.placeholder.com/150', 'in_cart' : TRUE },
     { 'title': '80\'s Music', 'description': 'Don\'t stop believing!' }
 ]
 @app.route('/items')
 def itemss_index():
     """Show all items."""
     return render_template('items.html', items=items)
+
+@app.route('/cart')
+def cart():
+    """display user's shopping cart"""
+    return render_template('cart.html', items=items.find())
